4f214af3d367a5cfcb1f0e6cfb80b6bfff9b1fee
anaberia <ana.beria3@gmail.com>
2022-10-08 18:12:32 +0400

    set states and handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
 
 function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello");
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
